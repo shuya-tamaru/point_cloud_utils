f3c38196b7df2a23b3da1ba7f34fd69ca698d8f3
shuya-tamaru <tamaru1013@gmail.com>
2025-03-25 09:53:16 +0900

    basic

--- main.py
+++ main.py
@@ -10,8 +10,9 @@ from src.visualization import visualize_segments
 
 
 def main():
-    input_file = "data/bunny.ply"
-    # input_file = "data/point_bi.ply"
+    # input_file = sample_pcd_data.path
+    # input_file = "data/bunny.ply"
+    input_file = "data/fragment.ply"
     output_dir = "results"
     try:
         if not os.path.exists(output_dir):
@@ -39,12 +40,12 @@ def main():
         filtered_pcd = filtered_pcd[0]
         filtered_pcd.paint_uniform_color([0,1,0])
 
-        voxel_size = 0.001
+        voxel_size = 0.01
 
         pcd_downsampled = filtered_pcd.voxel_down_sample(voxel_size = voxel_size)
 
         nn_distance = np.mean(pcd.compute_nearest_neighbor_distance())
-        radius_normals = nn_distance * 4
+        radius_normals = nn_distance * 8
         pcd_downsampled.estimate_normals(
             search_param = o3d.geometry.KDTreeSearchParamHybrid(radius = radius_normals, max_nn = 16),
             fast_normal_computation = True
@@ -57,9 +58,11 @@ def main():
         up = [ 0.0026773784657139086, 0.9999415671351739, -0.010473488425733157 ]
         zoom = 0.68599999999999994
 
+        # o3d.visualization.draw_geometries([pcd_downsampled],zoom=zoom,front=front,lookat=lookat,up=up)
+
         pcd = pcd_downsampled
 
-        pt_to_plane = 0.002
+        pt_to_plane = 0.02
         plane_model, inliers = pcd.segment_plane(distance_threshold=pt_to_plane, ransac_n=3, num_iterations=1000)
         [a,b,c,d] = plane_model
         print(f"Plane equation: {a:.2f}x + {b:.2f}y + {c:.2f}z + {d:.2f} = 0")
@@ -71,7 +74,7 @@ def main():
 
 
         max_plane_idx = 6
-        pt_to_plane_dist = 0.002
+        pt_to_plane_dist = 0.02
 
         segment_models = {}
         segments = {}
@@ -79,11 +82,18 @@ def main():
 
         for i in range(max_plane_idx):
             colors = plt.get_cmap("tab20")(i)
-            segment_models[i], inliers = rest.segment_plane(distance_threshold = pt_to_plane_dist, ransac_n = 3, num_iterations = 1000)
+            segment_models[i], inliers = rest.segment_plane(distance_threshold = pt_to_plane_dist, ransac_n = 3, num_iterations = 10000)
             segments[i] = rest.select_by_index(inliers)
             segments[i].paint_uniform_color(list(colors[:3]))
             rest = rest.select_by_index(inliers,invert=True)
             print("pass", i , "/", max_plane_idx, " done.")
+        
+        labels = np.array(rest.cluster_dbscan(eps=0.1, min_points=10))
+        max_label = labels.max()
+        print(f"point cloud has {max_label + 1} clusters.")
+        colors = plt.get_cmap("tab10")(labels / (max_label if max_label > 0 else 1))
+        colors[labels < 0] = 0
+        rest.colors = o3d.utility.Vector3dVector(colors[:, :3])
 
         o3d.visualization.draw_geometries([segments[i] for i in range(max_plane_idx)] + [rest],zoom=zoom,front=front,lookat=lookat,up=up)
 
